19d35287ae2b31f4bebe13447590201c946a8d2f
Revennat <25420033@uttcampus.edu.mx>
2026-09-20 19:18:03 -0600

    Se hizo la funcion BanderaItalia, el cual se implementa en el Main Activity

--- app/src/main/java/com/example/practicas/MainActivity.kt
+++ app/src/main/java/com/example/practicas/MainActivity.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.practicas.ui.theme.PracticasTheme
 
@@ -20,10 +26,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             PracticasTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    BanderaItalia(Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,13 +34,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun BanderaItalia(modifier: Modifier = Modifier) {
+    Row(modifier.fillMaxSize()) {
+        Column(Modifier.fillMaxHeight().weight(2f).background(Color.Green)) { }
+        Column(Modifier.fillMaxHeight().weight(2f).background(Color.White)) { }
+        Column(Modifier.fillMaxHeight().weight(2f).background(Color.Red)) { }
+    }
 }
-
+/*
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
@@ -45,3 +49,4 @@ fun GreetingPreview() {
         Greeting("Android")
     }
 }
+ */
